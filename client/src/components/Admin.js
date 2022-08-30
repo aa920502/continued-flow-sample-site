eb3bfadd210e71ee9137ae9331df208ff9a11373
aa920502 <davidlucs@gmail.com>
2022-08-30 09:46:54 -0700

    Add multi select dropdown as well as grabbing selected fields at real time, move backend call to admin page for now

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "./NavBar";
+import axios from "axios";
 
 function Admin() {
+  const [backendData, setBackendData] = useState([{}]);
+
+  // execute once when the page loads, GET /api endpoint and set data, don't need full path here because we set up the proxy
+  // Example using fetch
+  // useEffect(() => {
+  //   fetch("/api")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setBackendData(data);
+  //     });
+  // }, []);
+
+  // useEfect is a hook that allows you to perform side effects in functional components.
+  // no array - runs after first render and after each update that gets made to the component
+  // empty array - runs only once during the first render, won't run when any updates happen
+  // array with value(s) inside - tracks the value(s) inside and run when the value(s) are changed/affected
+  useEffect(() => {
+    axios.get("/api").then(function (response) {
+      setBackendData(response.data);
+    });
+  }, []);
+
   return (
     <div>
       <NavBar />
+      <div className="w3-container w3-content w3-center w3-padding-64">
+        <div class="container">
+          {typeof backendData.users === "undefined" ? (
+            <p>Loading...</p>
+          ) : (
+            backendData.users.map((user, i) => <p key={i}>{user}</p>)
+          )}
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/components/Tutorial.js
+++ client/src/components/Tutorial.js
@@ -1,40 +1,130 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import NavBar from "./NavBar";
-import axios from "axios";
+
+import "./formik-demo.css";
+import { withFormik } from "formik";
+import * as Yup from "yup";
+
+import { DisplayFormikState } from "./code-helper";
+import MySelect from "./MySelect";
 
 function Tutorial() {
-  const [backendData, setBackendData] = useState([{}]);
+  const formikEnhancer = withFormik({
+    validationSchema: Yup.object().shape({
+      lead_id: Yup.string()
+        .length(15, "this needs to be 15 characters")
+        .required("Lead ID is required!"),
+      fields: Yup.array()
+        .min(1, "Select at least 1 field to prefill")
+        .of(
+          Yup.object().shape({
+            label: Yup.string().required(),
+            value: Yup.string().required(),
+          })
+        ),
+    }),
+    mapPropsToValues: (props) => ({
+      lead_id: "",
+      fields: [],
+    }),
+    handleSubmit: (values, { setSubmitting }) => {
+      const payload = {
+        ...values,
+        fields: values.fields.map((t) => t.value),
+      };
+      setTimeout(() => {
+        alert(JSON.stringify(payload, null, 2));
+        setSubmitting(false);
+      }, 1000);
+    },
+    setFieldValue: (values) => {
+      console.log("test");
+    },
+    displayName: "MyForm",
+  });
 
-  // execute once when the page loads, GET /api endpoint and set data, don't need full path here because we set up the proxy
-  // Example using fetch
-  // useEffect(() => {
-  //   fetch("/api")
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setBackendData(data);
-  //     });
-  // }, []);
+  const MyForm = (props) => {
+    const {
+      values,
+      touched,
+      dirty,
+      errors,
+      handleChange,
+      handleBlur,
+      handleSubmit,
+      handleReset,
+      setFieldValue,
+      setFieldTouched,
+      isSubmitting,
+    } = props;
+    console.log(props.values);
 
-  // useEfect is a hook that allows you to perform side effects in functional components.
-  // no array - runs after first render and after each update that gets made to the component
-  // empty array - runs only once during the first render, won't run when any updates happen
-  // array with value(s) inside - tracks the value(s) inside and run when the value(s) are changed/affected
-  useEffect(() => {
-    axios.get("/api").then(function (response) {
-      setBackendData(response.data);
-    });
-  }, []);
+    let output = parseInput(props.values.fields).join();
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="lead_id" style={{ display: "block" }}>
+          Lead ID
+        </label>
+        <input
+          id="lead_id"
+          placeholder="Enter your lead ID"
+          value={values.lead_id}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        {String(values.lead_id).length > 0 &&
+          String(values.lead_id).length != 15 && (
+            <div style={{ color: "red", marginTop: ".5rem" }}>
+              Please enter 15 digit lead ID
+            </div>
+          )}
+        <MySelect
+          value={values.fields}
+          onChange={setFieldValue}
+          onBlur={setFieldTouched}
+          error={errors.fields}
+          touched={touched.fields}
+        />
+        <button
+          type="button"
+          className="outline"
+          onClick={handleReset}
+          disabled={!dirty || isSubmitting}
+        >
+          Reset
+        </button>
+        <button type="submit" disabled={isSubmitting}>
+          Submit
+        </button>
+
+        <DisplayFormikState {...props} />
+        <div>
+          <h1>Fields to prefill: {output}</h1>
+        </div>
+      </form>
+    );
+
+    function parseInput(fields) {
+      let result = [];
+      if (fields.length === 0) {
+        return result;
+      }
+      for (let i = 0; i < fields.length; i++) {
+        let value = fields[i].value;
+        result.push(value);
+      }
+      return result;
+    }
+  };
+
+  const MyEnhancedForm = formikEnhancer(MyForm);
+
   return (
     <div>
       <NavBar />
-      <div className="w3-container w3-content w3-center w3-padding-64">
-        <div class="container">
-          {typeof backendData.users === "undefined" ? (
-            <p>Loading...</p>
-          ) : (
-            backendData.users.map((user, i) => <p key={i}>{user}</p>)
-          )}
-        </div>
+      <div className="w3-container w3-content w3-padding-64">
+        <MyEnhancedForm />
       </div>
     </div>
   );
