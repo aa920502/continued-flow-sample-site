ccd1a6f58b8f58ab4290a41bd9fcd6e20e769a42
Akshaya Ramaswamy <akshayaram95@fb.com>
2022-09-05 20:17:33 -0700

    Reading from the database and displaying in the admin page

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,10 +1,13 @@
 import React from "react";
 import NavBar from "./NavBar";
+import AdminTable from "./AdminTable";
+
 
 function Admin() {
   return (
     <div>
       <NavBar />
+      <AdminTable />
     </div>
   );
 }
